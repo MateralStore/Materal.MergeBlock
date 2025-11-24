3ba7d7cf67821eec5d5d2f7a2fde356236e1cea2
陈明旭 <cloomcmx1554@hotmail.com>
2025-11-24 16:51:36 +0800

    feat: 初始化 MergeBlock.WebTest 项目并添加基础配置

---
+++ MergeBlock.WebTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using Materal.MergeBlock.Web;
+
+namespace MergeBlock.WebTest;
+
+/// <summary>
+/// 主程序
+/// </summary>
+public class Program
+{
+    /// <summary>
+    /// 入口
+    /// </summary>
+    /// <param name="args"></param>
+    /// <returns></returns>
+    public static async Task Main(string[] args) => await MergeBlockProgram.RunAsync(args);
+}
